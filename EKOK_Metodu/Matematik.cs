160bef31d608124d649f167129f4db91c8050519
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 18:38:42 +0300

    Proje dosyası ekle.

---
+++ EKOK_Metodu/Matematik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EKOK_Metodu
+{
+    class Matematik
+    {
+        public static int OKEK(int s1,int s2)
+        {
+            int s = 1;
+            while (s1!=1 && s2!=2)
+            {
+                int bol = 2;
+                for (int i = 1; i <= (s1>s2 ? s1:s2 ); i++)
+                {
+                    if (s1 % bol == 0 || s2 % bol == 0)
+                    {
+                        s *= bol;
+                        if (s1 % bol == 0)
+                            s1 /= bol;
+                        if (s2 % bol == 0)
+                            s2 /= bol;
+                    }
+                    else
+                        bol++;
+                }
+            }
+            return s;
+        } 
+    }
+}
---
+++ EKOK_Metodu/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EKOK_Metodu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double s1 = Convert.ToDouble(Console.ReadLine());
+            double s2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("OKEK({0},{1})= {2}",s1,s2, Matematik.OKEK((int) s1,(int) s2));
+        }
+    }
+}
